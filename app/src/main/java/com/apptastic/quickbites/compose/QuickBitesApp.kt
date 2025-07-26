3c134ebb19f5f45d29d7a56e41e1755d5280fcfa
shivaram <app.apptastic@gmail.com>
2025-07-26 15:09:56 +0530

    added bottom navigation

--- app/src/main/java/com/apptastic/quickbites/compose/QuickBitesApp.kt
+++ app/src/main/java/com/apptastic/quickbites/compose/QuickBitesApp.kt
@@ -1,8 +1,15 @@
 package com.apptastic.quickbites.compose
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.runtime.Composable
@@ -10,13 +17,15 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.apptastic.quickbites.MainApplication
+import com.apptastic.quickbites.R
 import com.apptastic.quickbites.navigation.QuickBitesDestinations
-import com.apptastic.quickbites.screens.payment.PaymentScreen
 import com.apptastic.quickbites.screens.address.AddressScreen
 import com.apptastic.quickbites.screens.auth.LoginScreen
 import com.apptastic.quickbites.screens.auth.LogoutScreen
@@ -26,10 +35,12 @@ import com.apptastic.quickbites.screens.contactus.ContactUsScreen
 import com.apptastic.quickbites.screens.help.HelpScreen
 import com.apptastic.quickbites.screens.home.HomeScreen
 import com.apptastic.quickbites.screens.orders.OrderScreen
+import com.apptastic.quickbites.screens.payment.PaymentScreen
 import com.apptastic.quickbites.screens.profile.ProfileScreen
 import com.apptastic.quickbites.screens.recommendations.RecommendationsScreen
 import com.apptastic.quickbites.screens.settings.SettingsScreen
 import com.apptastic.quickbites.screens.welcome.WelcomeScreen
+import com.apptastic.quickbites.utils.AppColor
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -48,7 +59,7 @@ fun QuickBitesApp() {
     }
 
     Scaffold(
-        bottomBar = {  },
+        bottomBar = { BottomNavigationBar(navController) },
         containerColor = MaterialTheme.colorScheme.background,
         snackbarHost = { SnackbarHost(networkManager.snackBarHostState) }
     ) { paddingValues ->
@@ -82,5 +93,49 @@ fun QuickBitesNavigation(navController: NavHostController, paddingValues: Paddin
         composable(QuickBitesDestinations.HelpFaq.route) { HelpScreen() }
         composable(QuickBitesDestinations.Settings.route) { SettingsScreen() }
         composable(QuickBitesDestinations.Logout.route) { LogoutScreen() }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val screens = listOf(
+        QuickBitesDestinations.Home to R.drawable.home_icon,
+        QuickBitesDestinations.MyOrders to R.drawable.my_orders_icon,
+        QuickBitesDestinations.BestSeller to R.drawable.favourites_icon,
+        QuickBitesDestinations.Recommendations to R.drawable.recommendations_icon,
+        QuickBitesDestinations.ContactUs to R.drawable.contact_us_icon
+    )
+    BottomAppBar(
+        containerColor = AppColor.colorE95322,
+        windowInsets = BottomAppBarDefaults.windowInsets,
+        contentPadding = BottomAppBarDefaults.ContentPadding,
+        modifier = Modifier.navigationBarsPadding().wrapContentHeight()
+    ) {
+        val navBackStackEntry = navController.currentBackStackEntryAsState() // gives the current screen in the navigation
+        val currentRoute = navBackStackEntry.value?.destination?.route // extracts the route name of the current screen
+
+        screens.forEach { (screen, icon) ->
+            NavigationBarItem(
+                selected = currentRoute == screen.route,
+                icon = {
+                    Icon(
+                        painter = painterResource(icon),
+                        contentDescription = screen.route, // Adjust padding as needed
+                    )
+                },
+                onClick = {
+                    if(screen.route != currentRoute)
+                    navController.navigate(screen.route) {
+                        launchSingleTop = true // Avoids multiple copies of the same destination
+                        restoreState = true // Restores the state of the destination when navigating back
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = AppColor.colorF5F5F5,
+                    indicatorColor = AppColor.colorE95322
+                ),
+                modifier = Modifier // Set background color for the item
+            )
+        }
     }
 }
--- app/src/main/java/com/apptastic/quickbites/utils/AppColor.kt
+++ app/src/main/java/com/apptastic/quickbites/utils/AppColor.kt
@@ -4,4 +4,11 @@ import androidx.compose.ui.graphics.Color
 
 object AppColor {
     val black = Color(0XFF000000)
+    val colorE95322 = Color(0XFFE95322)
+    val colorF5F5F5 = Color(0XFFF5F5F5)
+    val colorF8F8F8 = Color(0XFFF8F8F8)
+    val colorF3E9B5 = Color(0XFFF3E9B5)
+    val color391713 = Color(0XFF391713)
+    val colorFFDECF = Color(0XFFFFDECF)
+    val colorF5CB58 = Color(0XFFF5CB58)
 }
