2e85fa87507aecf80f7db8573f3f8f955bed99ef
shivaram <app.apptastic@gmail.com>
2025-07-26 13:40:07 +0530

    added all required screens, WIP

---
+++ app/src/main/java/com/apptastic/quickbites/screens/contactus/ContactUsScreen.kt
@@ -0,0 +1,9 @@
+package com.apptastic.quickbites.screens.contactus
+
+import androidx.compose.runtime.Composable
+import com.apptastic.quickbites.compose.components.Header1
+
+@Composable
+fun ContactUsScreen() {
+     Header1(text = "Contact Us Screen")
+}
---
+++ app/src/main/java/com/apptastic/quickbites/screens/orders/OrdersScreen.kt
@@ -0,0 +1,9 @@
+package com.apptastic.quickbites.screens.orders
+
+import androidx.compose.runtime.Composable
+import com.apptastic.quickbites.compose.components.Header1
+
+@Composable
+fun OrderScreen() {
+    Header1(text = "Order Screen")
+}
